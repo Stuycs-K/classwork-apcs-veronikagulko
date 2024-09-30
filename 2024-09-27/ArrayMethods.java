3f2161ab9c308b0029022b84ecf7481a3e2ce9a3
Veronika Gulko <veronika.s.gulko@gmail.com>
2024-09-30 08:23:06 -0400

    added arr to string func and return copy

--- 2024-09-27/ArrayMethods.java
+++ 2024-09-27/ArrayMethods.java
@@ -1,2 +1,37 @@
 public class ArrayMethods{
+    public static void main(String[] args) {
+        int[] test1 = {1, 2, 3, 4, 5, 6};
+        int[] test2 = returnCopy(test1);
+        boolean test3 = tester(test1, test2);
+        result(test3);
+    }
+    public static String aryToString(int[] nums){
+        String result = "{";
+        for(int i = 0; i < nums.length - 1; i++){
+            result += nums + ", ";
+        }
+        result = nums[-1] + "}";
+        return(result);
+    }
+    public static int[] returnCopy(int[]ary){
+        int[] result = new int[ary.length];
+        for(int i = 0; i < ary.length; i++){
+            result += ary[i];
+        }
+        return(result);
+    }
+    public static int[] tester(int[]a, int[]b){
+        boolean test1 = true;
+        boolean test2 = fase;
+        for(int i = 0; i < a.length; i++){
+            if(a[i] != b[i]){
+                test1 = false;
+            }
+        }
+        a[0] = 0;
+        if(a[0] == b[0]){
+            test2 = true;
+        }
+        return(test1 && test2);
+    }
 }
