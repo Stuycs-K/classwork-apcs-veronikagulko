dd5cb8acec99164ac777d2125a7a3f6ac56655ba
Veronika Gulko <veronika.s.gulko@gmail.com>
2024-10-01 12:42:03 -0400

    added test cases

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -1,15 +1,15 @@
 //Hangqi, hangqih@nycstudents.net; Veronika, vgulko60@stuy.edu
 public class ArrayMethods{
   public static void main(String[] args){
-    int[] 1dArray = new int[]{1, 2, 3, 4, 5};
-    System.out.println("1DArray to String test: " + aryToString(1dArray));
-    int[][] 2dArray = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    System.out.println("2DArray to String test 1: " + aryToString(2dArray));
-    int[][] 2dArray2 = new int[][]{{1, 2, 3}, {4, 6}, {7, 8}};
-    System.out.println("2DArray to String test 2: " + aryToString(aryToString(2dArray2)));
-    System.out.println("Sum of all elements in 2D array test 1: " + aryToString(arr2DSum(2dArray)));
-    System.out.println("Sum of all elements in 2D array test 2: " + aryToString(arr2DSum(2dArray2)));
-    System.out.println("Transpose test " + aryToString(swapRC(2dArray)));
+    int[] oneDArray = {1, 2, 3, 4, 5};
+    System.out.println("oneDArray to String test " + aryToString(oneDArray));
+    int[][] twodArray = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    System.out.println("twodArray to String test 1: " + aryToString(twodArray));
+    int[][] twodArray2 = new int[][]{{1, 2, 3}, {4, 6}, {7, 8}};
+    System.out.println("twodArray2 to String test 2: " + aryToString(twodArray2));
+    System.out.println("Sum of all elements in 2D array test 1: " + (arr2DSum(twodArray)));
+    System.out.println("Sum of all elements in 2D array test 2: " + (arr2DSum(twodArray2)));
+    System.out.println("Transpose test " + aryToString(swapRC(twodArray)));
   }
 
   public static String aryToString(int[] nums){
@@ -30,20 +30,7 @@ public class ArrayMethods{
     result += "}";
     return(result);
   }
-  public static String aryToString(int[][] nums){
-    String total = "{";
-    String result = "{";
-    for(int i = 0; i < nums.length; i++){
-      for(int j = 0; j < nums[i].length; j++){
-        result += nums[i][j] + ", ";
-      }
-      result += nums[nums.length - 1][nums.length - 1] + "}";
-      return result;
-    }
-    total = total.substring(0,-2);
-    total += "}";
-    return(total);
-  }
+ 
 public static int arr2DSum(int[][]nums) {
   int sum = 0;
   for(int i = 0; i < nums.length; i++){
