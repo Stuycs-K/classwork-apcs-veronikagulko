d679b4f85a810309551221de00b119fac5d2c3b2
Veronika Gulko <veronika.s.gulko@gmail.com>
2024-10-07 09:06:54 -0500

    read thru instructions, added all func names

---
+++ 2024-10-07-2DArrayMethods/ArrayDemo.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+public static ArrayDemo{
+  public static voic main(String[]args){
+    //tests
+  }
+  public static String arrToString(int[] ary){
+    // array to string one d array
+  }
+  public static String arrToString(int[][] ary){
+    // array to string two d array
+  }
+  public static int countZeroes2D(int[][] nums){
+    // count how many zeroes are in array
+  }
+  public static int arr2DSum(int[][] nums){
+    // calculate sum, use nested loop
+  }
+  public static void replaceNegative(int[][] vals){
+    // same as hw
+  }
+  public static int[][] swapRC(int[][] nums){
+    // rotate the array
+  }
+  public static String htmlTable(int[][] nums){
+    // place tr tags around each row, place td tags aound each value (make helper func)
+    // place table tag around entirety
+  }
+}
