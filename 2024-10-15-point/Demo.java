d2ecf81da2134d5013db0ecc72eade6de3c4a2d6
Veronika Gulko <veronika.s.gulko@gmail.com>
2024-10-31 10:02:23 -0400

    tested printing out next double

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -12,17 +12,15 @@ public class Demo{
     Point p1 = new Point(1,1);
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
-    Point p4 = new Point(0,5);
-    Point p5 = new Point(5,10);
-    Point p6 = new Point(5,5);
+    Point t1 = new Point(0,0);
+    Point t2 = new Point(0,4);
+    Point t3 = new Point(2,(2*Math.sqrt(3)));
     System.out.println( p3);
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
-    System.out.println( p6);
-    System.out.println( distance(p3,p4));
-    System.out.println( Point.distance(p3,p4));
-    System.out.println( p3.distanceTo(p4));
-    ;
+    System.out.println( distance(t3,t2));
+    System.out.println( Point.distance(t2,t3));
+    System.out.println( t3.distanceTo(t2));
   }
 }
--- 2024-10-31-Advent/Main.java
+++ 2024-10-31-Advent/Main.java
@@ -3,9 +3,14 @@ class Main {
     public static void main(String[] args){
         String text = "This is a sample \n Of how to read \n text";
         Scanner sci = new Scanner("This is a bunch of words");
+        /*
         while(sci.hasNext()){
             System.out.println(sci.next());
         }
-        Scanner sc1 = new Scanner("10.0 1.04 99.0 -3.0 4.2");
+        */
+        Scanner sc2 = new Scanner("10.0 1.04 99.0 -3.0 4.2");
+        while(sc2.hasNextDouble()){
+            System.out.println(sc2.nextDouble());
+        }
     }
 }
