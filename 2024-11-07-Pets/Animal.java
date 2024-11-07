9c7856c79d24bd7c746e2add7560c4166e92386b
Veronika Gulko <veronika.s.gulko@gmail.com>
2024-11-07 08:54:47 -0600

    added speak

--- 2024-11-07-Pets/Animal.java
+++ 2024-11-07-Pets/Animal.java
@@ -4,7 +4,9 @@ public class Animal {
   private String name;
 
   public Animal(String noise, int age, String name){
-    //initialize the values (hint use this.varname to clarify)
+    this.noise = noise;
+    this.age = age;
+    this.name = name;
   }
 
   //2.
@@ -17,12 +19,16 @@ public class Animal {
   Note: you MUST use getName() getNoise() here! You will see why later.
   */
   public void speak(){
-
+      System.out.println("My name is" + getName() + ". \n" + "I am " + getAge() + " years old. \n" + "I say" + getNoise() + ".");
   }
 
   public String getName(){
     return name;
   }
+  public int getAge(){
+    return age;
+  }
   public String getNoise(){
     return noise;
   }
+}
