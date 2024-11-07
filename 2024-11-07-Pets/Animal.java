b5278c37a88275fa87a40542e058892d318beccf
Veronika Gulko <veronika.s.gulko@gmail.com>
2024-11-07 08:45:28 -0600

    added animal

---
+++ 2024-11-07-Pets/Animal.java
@@ -0,0 +1,28 @@
+public class Animal {
+  private String noise;
+  private int age;
+  private String name;
+
+  public Animal(String noise, int age, String name){
+    //initialize the values (hint use this.varname to clarify)
+  }
+
+  //2.
+  /*speak() should print the following message replacing
+  the ?'s with name/age/noise:
+  My name is ?.
+  I am ? years old.
+  I say ?.
+
+  Note: you MUST use getName() getNoise() here! You will see why later.
+  */
+  public void speak(){
+
+  }
+
+  public String getName(){
+    return name;
+  }
+  public String getNoise(){
+    return noise;
+  }
