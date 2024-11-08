8b5d08dc3ed9e3d94d8901d58c1d79945ab001ed
Veronika Gulko <veronika.s.gulko@gmail.com>
2024-11-08 08:27:47 -0500

    overide the getname

--- 2024-11-07-Pets/Bird.java
+++ 2024-11-07-Pets/Bird.java
@@ -25,6 +25,9 @@ public class Bird extends Animal {
   //7:Override the getName() method so that a bid's name is "The Mighty NAME" instead of "NAME"
   //e.g. "The Mighty Polly" instead of just "Polly"
   //You can call the superclass' getName method using the syntax: super.getName()
+  public String getName(){
+    return ("The Mighty " + super.getName());
+  }
 
   //Step 8:In Driver.java answer: What is the difference before and after you override this method?
   //What does this tell us about methods that are overridden?
