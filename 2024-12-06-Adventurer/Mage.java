e598738e1df9bc298393d81308bee2e9b9a4d550
Veronika Gulko <veronika.s.gulko@gmail.com>
2024-12-09 10:05:50 -0500

    added applydamange + subtractec magic

--- 2024-12-06-Adventurer/Mage.java
+++ 2024-12-06-Adventurer/Mage.java
@@ -1,9 +1,8 @@
 
-import java.awt.RenderingHints;
 
 public class Mage extends Adventurer{
     private int magic;
-    private int maxMagic = 0;
+    private int maxMagic = 10;
 
     public Mage (String name){
         super(name);
@@ -11,7 +10,7 @@ public class Mage extends Adventurer{
     }
     public Mage (String name, int hp){
         super(name, hp);
-        this.magic = 0;
+        this.magic = 6;
     }
 
     public String getSpecialName(){
@@ -27,33 +26,37 @@ public class Mage extends Adventurer{
     public int getSpecialMax(){
         return maxMagic;
     }
-    public void applyDamage(){
-        this.magic--;
-    }
   /*
     all adventurers must have a way to attack enemies and
     support their allys
   */
   //hurt or hinder the target adventurer
     public String attack(Adventurer other){
+        other.applyDamage(3);
+        this.magic--;
         return this.getName() + " attacked " + other.getName() + " with " + 5 + "  damage";
-        other.applyDamage();
     }
 
     
 
   //heall or buff the target adventurer
     public String support(Adventurer other){
+        other.restoreSpecial(2);
+        this.magic--;
         return this.getName() + " healed " + other.getName() + " with a spell";
     }
 
   //heall or buff self
     public String support(){
+        this.restoreSpecial(2);
+        this.magic--;
         return this.getName() + " healed themselves with a spell";
     }
 
   //hurt or hinder the target adventurer, consume some special resource
     public  String specialAttack(Adventurer other){
+        other.applyDamage(5);
+        this.magic--;
         return this.getName() + " attacked  " + other.getName() + " with a super spell of "
 + 10 + " damage";    }
 
