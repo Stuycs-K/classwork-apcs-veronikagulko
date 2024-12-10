4817942e63f64b5c3ea6c30c3ffb83300396fa77
Veronika Gulko <veronika.s.gulko@gmail.com>
2024-12-10 09:09:05 -0500

    game

---
+++ 2024-12-06-Adventurer/Game.java
@@ -0,0 +1,10 @@
+public class Game{
+    Scanner userInput = new Scanner(System.in);
+    while(alive){
+        System.out.print(p1.getName() + ", " + p1.getHP() + "/" + p1.getHP() + " HP, " + p1.getSpecial() + "/" + p1.getSpecialMax() + " " + p1.getSpecialName());
+        System.out.print(p2.getName() + ", " + p2.getHP() + "/" + p2.getHP() + " HP, " + p2.getSpecial() + "/" + p2.getSpecialMax() + " " + p2.getSpecialName());
+        System.out.println("What is your type:");
+        String action = user.Input(nextLine());
+
+    }
+}
